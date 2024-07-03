7e554bb48fd8b4658b4e22df7adfd6f115ffa23e
TrHoangTin <trantin064@gmail.com>
2024-07-03 19:39:23 +0700

    DoAnjava

---
+++ src/main/java/com/example/WebBanHang/controller/OrderController.java
@@ -0,0 +1,69 @@
+package com.example.WebBanHang.controller;
+import com.example.WebBanHang.config.VNPAYService;
+import com.example.WebBanHang.model.CartItem;
+import com.example.WebBanHang.service.CartService;
+import com.example.WebBanHang.service.OrderService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+    @Autowired
+    private VNPAYService vnPayService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private CartService cartService;
+    @GetMapping("/checkout")
+    public String checkout() {
+        return "/cart/checkout";
+    }
+    @PostMapping("/submit")
+    public String submitOrder(String customerName) {
+        List<CartItem> cartItems = cartService.getCartItems();
+        if (cartItems.isEmpty()) {
+            return "redirect:/cart"; // Redirect if cart is empty
+        }
+        orderService.createOrder(customerName, cartItems);
+        return "redirect:/order/confirmation";
+    }
+    @GetMapping("/confirmation")
+    public String orderConfirmation(Model model) {
+        model.addAttribute("message", "Your order has been successfully placed.");
+        return "cart/order-confirmation";
+    }
+
+    @PostMapping("/submitOrder")
+    public String submidOrder(@RequestParam("amount") int orderTotal,
+                              @RequestParam("orderInfo") String orderInfo,
+                              HttpServletRequest request){
+        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        String vnpayUrl = vnPayService.createOrder(request, orderTotal, orderInfo, baseUrl);
+        return "redirect:" + vnpayUrl;
+    }
+
+    @GetMapping("/vnpay-payment-return")
+    public String paymentCompleted(HttpServletRequest request, Model model){
+        int paymentStatus =vnPayService.orderReturn(request);
+
+        String orderInfo = request.getParameter("vnp_OrderInfo");
+        String paymentTime = request.getParameter("vnp_PayDate");
+        String transactionId = request.getParameter("vnp_TransactionNo");
+        String totalPrice = request.getParameter("vnp_Amount");
+
+        model.addAttribute("orderId", orderInfo);
+        model.addAttribute("totalPrice", totalPrice);
+        model.addAttribute("paymentTime", paymentTime);
+        model.addAttribute("transactionId", transactionId);
+
+        return "cart/orderSuccess";
+    }
+}
